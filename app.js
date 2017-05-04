141b3cc1b947b7382e49d9da3f76007e537455b0
Rachel <racheldepriest1@gmail.com>
2017-05-04 11:41:05 -0500

    Able to get estimates for each issue type

--- app.js
+++ app.js
@@ -4,50 +4,91 @@ var prompt = require('readline-sync');
 var findData = function() {
 	this.allData = function(){
 		fs.readFile('exampleData/allData.js', 'utf8',(err, data) => {
-		  var parsedAllData = JSON.parse(data)
+		  var parsedData = JSON.parse(data);
 		  if (err){
 		  	throw err;
 		  } 
-		 console.log(parsedAllData)
+		 console.log(parsedData)
 		});
 	}
 	this.dataByBugIssueType = function(){
 		fs.readFile('exampleData/dataByBugIssueType.js', 'utf8',(err, data) => {
-		  var parsedData = JSON.parse(data)
+		 var parsedData = JSON.parse(data);	
 		  if (err){
 		  	throw err;
 		  } 
-		 console.log(parsedData)
+		console.log(parsedData)
 		});
 	}	
 	this.dataByStoryIssueType = function(){
 		fs.readFile('exampleData/dataByStoryIssueType.js', 'utf8',(err, data) => {
-		  var parsedData = JSON.parse(data)
+		  var parsedData = JSON.parse(data);
 		  if (err){
 		  	throw err;
 		  } 
 		 console.log(parsedData)
 		});
-	}	
+	}		
 }
 
-findData.prototype.isWhatTypeOfData = function(){
-	
+findData.prototype.findEstimateForAll = function(){
+	fs.readFile('exampleData/allData.js', 'utf8',(err, data) => {
+		var parsedData = JSON.parse(data);
+		if (err) {
+			throw err; 
+		}
+		var estimate = 0;
+		for(var i = 0; i < parsedData.length; i++){
+				if(parsedData[i].estimate){
+					estimate = +estimate + +parsedData[i].estimate;
+				}
+			}
+		console.log('This is estimate:', estimate)
+	})
 }
 
-findData.prototype.DataNotFound = function (){
+findData.prototype.findEstimateForBug = function(){
+	fs.readFile('exampleData/dataByBugIssueType.js', 'utf8',(err, data) => {
+		var parsedData = JSON.parse(data);
+		if (err) {
+			throw err; 
+		}
+		var estimate = 0;
+		for(var i = 0; i < parsedData.length; i++){
+				estimate = +estimate + +parsedData[i].estimate;
+			}
+		console.log('This is estimate:', estimate)
+	})
+}
+
+findData.prototype.findEstimateForStory = function(){
+	fs.readFile('exampleData/dataByStoryIssueType.js', 'utf8',(err, data) => {
+		var parsedData = JSON.parse(data);
+		if (err) {
+			throw err; 
+		}
+		var estimate = 0;
+		for(var i = 0; i < parsedData.length; i++){
+				estimate = +estimate + +parsedData[i].estimate;
+			}
+		console.log('This is estimate:', estimate)
+	})
+}
+
+findData.prototype.dataNotFound = function (){
 	console.log('Data not found, please try another query');
 } 
 
-findData.prototype.search = function(){
-	// this.allData();
-	// this.DataNotFound();
-	//this.dataByBugIssueType();
-	this.dataByStoryIssueType();
+findData.prototype.prompt = function(){
+	var ask = prompt.question('What kind of data are you looking for? Type in all, bug, or story to see the issues estimate time.');
+	
 }
 
 
+findData.prototype.search = function(){
+	//this.prompt();	
+	this.findEstimateForAll();
+}
+
 var query = new findData();
 query.search();
-
-
